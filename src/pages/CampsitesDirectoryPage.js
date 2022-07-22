a043f901005036a5c3d007da7e349d183d15d786
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2022-07-22 16:14:09 -0500

    Breadcrumbs SubHeader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,12 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
 import CommentList from '../features/comments/CommentList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
     return (
         <Container>
+            <SubHeader current='Home'/>
             <CampsitesList />
             <CommentList />
         </Container>
