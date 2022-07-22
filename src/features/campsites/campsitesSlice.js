85eee80c41e0ef885664afdfd34ecd68eeb7abf3
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2022-07-22 10:39:01 -0500

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -11,3 +11,7 @@ export const selectAllCampsites = () => {
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === id);
 };
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
