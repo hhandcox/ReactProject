66d3122dbe86f5d679251ba000160ffdd10c58d6
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2022-08-04 20:24:55 -0500

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { promotionsReducer } from '../features/promotions/promotionSlice';
@@ -10,5 +11,6 @@ export const store = configureStore({
     comments: commentsReducer,
     partners: partnersReducer,
     promotions: promotionsReducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
