0146d86e0e78dbaf54da4e87e837b12f103d77a2
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2022-08-07 13:53:04 -0500

    Week 4 Workshop

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,15 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { promotionsReducer } from '../features/promotions/promotionSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
